58ea62cfc483e5121271967663634a3492f6b9dc
younessbennaj <youness.bennaj@gmail.com>
2024-09-18 18:50:32 +0900

    config: add prettier and eslint

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,14 +1,17 @@
 import SignUpForm from "../SignUpForm";
 
 function SignUp() {
-    return (
-        <>
-            <h2>Create your account</h2>
-            <p>Enter the fields below to get started</p>
-            <SignUpForm />
-            <p>You already have an account? <span style={{color: "#007bff"}}>Log in</span></p>
-        </>
-    )
+  return (
+    <>
+      <h2>Create your account</h2>
+      <p>Enter the fields below to get started</p>
+      <SignUpForm />
+      <p>
+        You already have an account?{" "}
+        <span style={{ color: "#007bff" }}>Log in</span>
+      </p>
+    </>
+  );
 }
 
-export default SignUp
+export default SignUp;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import SignUp from './components/SignUp/index.tsx';
-import App from './App.tsx';
-import SignIn from './components/SignIn/index.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignUp from "./components/SignUp/index.tsx";
+import App from "./App.tsx";
+import SignIn from "./components/SignIn/index.tsx";
 
 const router = createBrowserRouter([
   {
@@ -14,15 +14,15 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignUp />,
-  }, 
+  },
   {
     path: "/signin",
     element: <SignIn />,
-  }
+  },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
