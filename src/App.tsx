b7758c0c555c9d83b792fe96992d2cbd091916da
younessbennaj <youness.bennaj@gmail.com>
2024-09-18 19:35:10 +0900

    config: add formating on commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,19 @@ import { Link } from "react-router-dom";
 
 export const supabase = createClient(
   `https://${import.meta.env.VITE_SUPABASE_PROJECT_ID}.supabase.co`,
-  `${import.meta.env.VITE_SUPABASE_ANON_KEY}`
+  `${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
 );
 
 function App() {
   useEffect(() => {
-          supabase.auth.getSession().then(({ data }) => {
+    supabase.auth.getSession().then(({ data }) => {
       console.log("data", data);
-          });
+    });
   }, []);
 
   return (
     <main>
-          <h1>Main Layout here</h1>
+      <h1>Main Layout here</h1>
       <Link to="/signup">Sign up</Link>
     </main>
   );
