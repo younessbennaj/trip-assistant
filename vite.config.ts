f7d80f9a2e920bbf6723e27738cdc75a11be930b
younessbennaj <youness.bennaj@gmail.com>
2024-09-30 15:52:15 +0900

    fix: add ts ignore because of plugin-react-swc error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   test: {
     globals: true,
     environment: "jsdom",
